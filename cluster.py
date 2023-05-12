cb99fbd3cf8ece7d9520e78c1465e9685b22fcf6
Quiracle <jsalattorres@gmail.com>
2023-05-13 01:18:35 +0200

    api returning group by seat

---
+++ cluster.py
@@ -0,0 +1,45 @@
+from sklearn.cluster import KMeans
+from scipy.spatial.distance import cdist
+from scipy.optimize import linear_sum_assignment
+import numpy as np
+import matplotlib.pyplot as plt
+from fastapi import FastAPI
+
+app = FastAPI()
+
+def getCoord(seat: str):
+  column = (ord(seat[0]) -65) if seat[0] < 'D' else (ord(seat[0]) -64.5)
+  row = (int(seat[1:]) -1)  * 1.1
+  return (column,row)
+
+
+def generate_positions(size):
+  import random
+  letters = 'ABCDEF'
+  players = []
+  for i in range(size):
+    seat = random.choice(letters)+str(random.randint(1,52))
+    players.append(seat)
+  return players
+
+
+def get_even_clusters(X, cluster_size):
+    n_init = 10
+    n_clusters = int(np.ceil(len(X)/cluster_size))
+    kmeans = KMeans(n_clusters, n_init=10)
+    kmeans.fit(X)
+    centers = kmeans.cluster_centers_
+    centers = centers.reshape(-1, 1, X.shape[-1]).repeat(cluster_size, 1).reshape(-1, X.shape[-1])
+    distance_matrix = cdist(X, centers)
+    clusters = linear_sum_assignment(distance_matrix)[1]//cluster_size
+    return clusters
+
+
+@app.post("/group")
+async def cluster(seats: list):
+    player_group = {}
+    players = np.array(list(map(getCoord, seats)))
+    groups = get_even_clusters(players, 4)
+    for player, group in zip(seats, groups):
+      player_group[player] = group
+    return player_group
